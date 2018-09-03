989b4b32b603379bee1bd8f0dc63f24de8b3f759
Sh4dowSoul <niklas.schnettler@gmail.com>
2018-09-03 02:09:13 +0200

    InstalledApps: Update GUI

--- app/src/main/java/com/apkupdater/adapter/InstalledAppAdapter.java
+++ app/src/main/java/com/apkupdater/adapter/InstalledAppAdapter.java
@@ -5,13 +5,10 @@ package com.apkupdater.adapter;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,9 +23,7 @@ import java.util.List;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-public class InstalledAppAdapter
-	extends RecyclerView.Adapter<InstalledAppAdapter.InstalledAppViewHolder>
-	implements View.OnClickListener
+public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapter.InstalledAppViewHolder>
 {
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -46,10 +41,10 @@ public class InstalledAppAdapter
 
 		private View mView;
         private TextView mName;
-        private TextView mPname;
         private TextView mVersion;
         private ImageView mIcon;
-        private Button mActionOneButton;
+        private ImageView mActionOneButton;
+        private TextView source;
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -62,28 +57,42 @@ public class InstalledAppAdapter
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		public void bind(
-			InstalledApp app
-		) {
+		public void bind(final InstalledApp app, int position) {
 		    // Get views
-            mName = ((TextView) mView.findViewById(R.id.installed_app_name));
-            mPname = ((TextView) mView.findViewById(R.id.installed_app_pname));
-            mVersion = ((TextView) mView.findViewById(R.id.installed_app_version));
-            mIcon = ((ImageView) mView.findViewById(R.id.installed_app_icon));
-            mActionOneButton = ((Button) mView.findViewById(R.id.action_one_button));
+            mName = mView.findViewById(R.id.installed_app_name);
+            mVersion = mView.findViewById(R.id.installed_app_version);
+            mIcon = mView.findViewById(R.id.installed_app_icon);
+            mActionOneButton = mView.findViewById(R.id.actionButton);
+            source = mView.findViewById(R.id.source);
 
             mName.setText(app.getName());
-            mPname.setText(app.getPname());
             mVersion.setText(app.getVersion());
+            source.setVisibility(View.GONE);
+			mActionOneButton.setImageResource(R.drawable.ic_eye_black_24dp);
+			mActionOneButton.setVisibility(View.VISIBLE);
+			mActionOneButton.setOnClickListener(v -> {
+				// Get the ignore list from the options
+				UpdaterOptions options = new UpdaterOptions(mContext);
+				List<String> ignore_list = options.getIgnoreList();
+
+				// If it's on the ignore remove, otherwise add it
+				if (ignore_list.contains(app.getPname())) {
+					ignore_list.remove(app.getPname());
+				} else {
+					ignore_list.add(app.getPname());
+				}
+
+				// Update the list
+				options.setIgnoreList(ignore_list);
+				notifyItemChanged(position);
+			});
 
             // Make the ignore overlay visible if this app is on the ignore list
             UpdaterOptions options = new UpdaterOptions(mContext);
             if (options.getIgnoreList().contains(app.getPname())) {
-                ViewCompat.setAlpha(mView, 0.50f);
-                mActionOneButton.setText(R.string.action_unignore_app);
+                mView.setAlpha(0.5f);
             } else {
-                ViewCompat.setAlpha(mView, 1.00f);
-                mActionOneButton.setText(R.string.action_ignore_app);
+                mView.setAlpha(1);
             }
 
             try {
@@ -92,8 +101,6 @@ public class InstalledAppAdapter
             } catch (PackageManager.NameNotFoundException e) {
             	mIcon.setImageResource(R.drawable.ic_android);
             }
-
-            mActionOneButton.setOnClickListener(mAdapter);
 		}
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -129,66 +136,16 @@ public class InstalledAppAdapter
 		ViewGroup parent,
 		int viewType
 	) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.installed_app_item, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.updater_item_new, parent, false);
 		return new InstalledAppViewHolder(v);
 	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	private CardView getInstalledAppViewParent(
-		View v
-	) {
-		while (v != null) {
-			v = (View) v.getParent();
-			if (v instanceof CardView) {
-				return (CardView) v;
-			}
-		}
-		return null;
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	@Override
-	public void onClick(
-		View view
-	) {
-		// Get the ignore list from the options
-		UpdaterOptions options = new UpdaterOptions(mContext);
-		List<String> ignore_list = options.getIgnoreList();
-
-		// Get the InstalledAppView parent
-        CardView parent = getInstalledAppViewParent(view);
-		if (parent == null) {
-			return;
-		}
-
-		InstalledApp app = mApps.get(mView.getChildLayoutPosition(parent));
-
-		// If it's on the ignore remove, otherwise add it
-		if (ignore_list.contains(app.getPname())) {
-			ignore_list.remove(app.getPname());
-		} else {
-			ignore_list.add(app.getPname());
-		}
-
-		// Update the list
-		options.setIgnoreList(ignore_list);
-
-		// Sort it
-		AnimationUtil.startSlideAnimation(mContext, mView);
-		mApps = InstalledAppUtil.sort(mContext, mApps);
-		notifyDataSetChanged();
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@Override
 	public void onBindViewHolder(
 		InstalledAppViewHolder holder,
 		int position
 	) {
-		holder.bind(mApps.get(position));
+		holder.bind(mApps.get(position), position);
 
         if (position == 0) {
             holder.setTopMargin(8);
